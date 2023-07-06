3f9e87d47210d709373c3d3bceeb99afe9e169cf
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-06 13:09:48 +0300

    Created view, controller

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <title>CRUD</title>
+</head>
+
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+@section('content')
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <h2 class="my-5 text-center">Laravel Ajax CRUD App</h2>
+            <div class="table-data">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>
+                                <a href="#" class="btn btn-success"><i class="las la-pen"></i></a>
+                                <a href="#" class="btn btn-danger"><i class="las la-trash"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+@endsection
